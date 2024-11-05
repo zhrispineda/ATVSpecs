7541818bdef36d414ee357799b89f9462d4b4db9
Chris <zhris@zhr.is>
2024-11-05 10:00:40 -0600

    Initial Commit

---
+++ ATVSpecs/ATVSpecsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ATVSpecsApp.swift
+//  ATVSpecs
+//
+//  Created by Chris on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ATVSpecsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
